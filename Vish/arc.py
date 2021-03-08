d74f90138e33a8f3d8d00ea8f13c006658b7986d
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2021-03-08 08:44:43 +0000

    a
    a

--- Vish/arc.py
+++ Vish/arc.py
@@ -24,6 +24,7 @@ TITLE = "MY GAME"
 CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 COIN_SCALING = 0.5
+PLAYER_MOVEMENT_SPEED = 5
 
 class Game(arcade.Window):
   def __init__(self):
@@ -34,39 +35,57 @@ class Game(arcade.Window):
     self.player_list = None
     self.player_sprite = None
 
+  def setup(self):
+    self.player_list = arcade.SpriteList()
+    self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+    self.coin_list = arcade.SpriteList(use_spatial_hash=True)
+    image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+    self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+    self.player_sprite.center_x = 64
+    self.player_sprite.center_y = 128
+    self.player_list.append(self.player_sprite)
+    for x in range(0, 1250, 64):
+        wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
+        wall.center_x = x
+        wall.center_y = 32
+        self.wall_list.append(wall)
+    coordinate_list = [[512, 96],
+                        [256, 96],
+                        [768, 96]]
+    for coordinate in coordinate_list:
+        wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
+        wall.position = coordinate
+        self.wall_list.append(wall)
 
+  def on_draw(self):
+    arcade.start_render()
+    self.wall_list.draw()
+    self.coin_list.draw()
+    self.player_list.draw()
 
+  def on_key_press(self, key, modifiers):
+    if key == arcade.key.UP or key == arcade.key.W:
+        self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+    elif key == arcade.key.DOWN or key == arcade.key.S:
+        self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+    elif key == arcade.key.LEFT or key == arcade.key.A:
+        self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+    elif key == arcade.key.RIGHT or key == arcade.key.D:
+        self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
-
-    def setup(self):
-      self.player_list = arcade.SpriteList()
-      self.wall_list = arcade.SpriteList(use_spatial_hash=True)
-      self.coin_list = arcade.SpriteList(use_spatial_hash=True)
-      image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
-      self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-      self.player_sprite.center_x = 64
-      self.player_sprite.center_y = 128
-      self.player_list.append(self.player_sprite)
-      for x in range(0, 1250, 64):
-          wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
-          wall.center_x = x
-          wall.center_y = 32
-          self.wall_list.append(wall)
-      coordinate_list = [[512, 96],
-                          [256, 96],
-                          [768, 96]]
-      for coordinate in coordinate_list:
-          wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
-          wall.position = coordinate
-          self.wall_list.append(wall)
-
-    def on_draw(self):
-      arcade.start_render()
-      self.wall_list.draw()
-      self.coin_list.draw()
-      self.player_list.draw()
-
-
+  def on_key_release(self, key, modifiers):
+    if key == arcade.key.UP or key == arcade.key.W:
+        self.player_sprite.change_y = 0
+    elif key == arcade.key.DOWN or key == arcade.key.S:
+        self.player_sprite.change_y = 0
+    elif key == arcade.key.LEFT or key == arcade.key.A:
+        self.player_sprite.change_x = 0
+    elif key == arcade.key.RIGHT or key == arcade.key.D:
+        self.player_sprite.change_x = 0
+    
+  #def on_update(self, delta_time):
+  #  self.physics_engine.update()
+      
 def main():
     window = Game()
     window.setup()
---
+++ fishy-game-main/README.md
@@ -0,0 +1,19 @@
+# fishy-game
+eat small fish, avoid big fish against the clock
+uses python arcade:  
+https://arcade.academy/index.html  
+https://github.com/pythonarcade/arcade 
+
+this is a prototype for when I make this game in unity.
+
+<img src="https://github.com/LiorAvrahami/fishy-game/blob/main/example%20image.png" width="500">
+
+
+# Controlls
+- restart - R
+- pause - space
+- move - arrows/wasd
+
+# Some Details
+- movment is smoothed out with finit acceleration and viscosity
+- the larger you get, the more slowly you will accelerate
